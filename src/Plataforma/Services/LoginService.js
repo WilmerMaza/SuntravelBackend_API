8a0d1b07780d41f0444758e5e62d2db3a6d7376e
WilmerMaza <wilmermaza10@gmail.com>
2023-11-14 22:47:32 -0500

    listo

--- src/Plataforma/Services/LoginService.js
+++ src/Plataforma/Services/LoginService.js
@@ -1,11 +1,29 @@
 const { AES, enc } = require("crypto-ts");
 const { User, Roll, Login } = require("../../models/Asociaciones");
 
+const fs = require("fs");
+const util = require("util");
+require("../../Utils/consts");
+
+const logFile = fs.createWriteStream("app.log", { flags: "a" });
+const logStdout = process.stdout;
+
+console.log = (message) => {
+  logFile.write(util.format(message) + "\n");
+  logStdout.write(util.format(message) + "\n");
+};
+
+console.error = (message) => {
+  logFile.write(util.format(message) + "\n");
+  logStdout.write(util.format(message) + "\n");
+};
+
 const login_function = async (Name, Password) => {
-  const { SECRETKEY } = process.env;
-  const pass = AES.decrypt(Password, enc.Utf8.parse(SECRETKEY)).toString(
+  console.log(global.SECRETKEY);
+  const pass = AES.decrypt(Password, enc.Utf8.parse(global.SECRETKEY)).toString(
     enc.Utf8
   );
+  console.log(pass);
   try {
     const dataBd = await Login.findOne({
       where: {
